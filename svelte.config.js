d410c92465fb0491209bde4b31a299910f3215bc
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-12-28 23:26:32 +0530

    -v

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+import adapter from '@sveltejs/adapter-vercel';
+
+export default {
 	kit: {
 		adapter: adapter()
 	}
 };
-
-export default config;
