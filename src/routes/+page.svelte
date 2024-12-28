f57acaf04c9809d96605139a16dbe5e2b1fb212c
Bhavya Panchal <panchalbhavya2210@gmail.com>
2024-12-28 23:12:09 +0530

    f

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script>
-  import Banner from "../components/Home/Banner.svelte";
-  import About from "../components/aboutCopm/About.svelte";
-  import "../style.css";
+	import Banner from '../components/Home/Banner.svelte';
+	import '../style.css';
 </script>
 
 <svelte:head>
-  <title>Home • Bhavya Panchal</title>
-  <meta name="description" content="My Webpage" />
+	<title>Home • Bhavya Panchal</title>
+	<meta name="description" content="My Webpage" />
 </svelte:head>
 <main>
-  <Banner />
-  <About />
+	<Banner />
 </main>
